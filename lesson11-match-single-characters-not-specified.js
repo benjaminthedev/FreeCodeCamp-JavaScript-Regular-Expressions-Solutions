ba4e0d0aa89bd38bbf107242563616e4956fa2fa
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-10-14 18:00:48 +0100

    new lessons

--- lesson11-match-single-characters-not-specified.js
+++ lesson11-match-single-characters-not-specified.js
@@ -2,4 +2,4 @@
 
 let quoteSample = "3 blind mice.";
 let myRegex = /[^0-9aeiou]/ig; // Change this line
-let result = quoteSample.match(myRegex); // Change this line
+let result = quoteSample.match(myRegex); // Change this lines
---
+++ lesson15-find-one-or-more-criminals-in-a-hunt.js
@@ -0,0 +1,9 @@
+//find-one-or-more-criminals-in-a-hunt
+
+// example crowd gathering
+let crowd = 'P1P2P3P4P5P6CCCP7P8P9';
+
+let reCriminals = /C+/; // Change this line
+
+let matchedCriminals = crowd.match(reCriminals);
+console.log(matchedCriminals);
