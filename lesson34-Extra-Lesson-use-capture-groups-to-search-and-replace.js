b9f3eb4d2e2ba912715053ab4f8227a1c490ce99
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2019-10-23 07:10:43 +0100

    update extra lessons from FCC

---
+++ lesson34-Extra-Lesson-use-capture-groups-to-search-and-replace.js
@@ -0,0 +1,6 @@
+//use-capture-groups-to-search-and-replace
+
+let huhText = "This sandwich is good.";
+let fixRegex = /good/; // Change this line
+let replaceText = "okey-dokey"; // Change this line
+let result = huhText.replace(fixRegex, replaceText);
---
+++ lesson35-Extra-Lesson-remove-whitespace-from-start-and-end.js
@@ -0,0 +1,5 @@
+//remove-whitespace-from-start-and-end
+
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g; // Change this line
+let result = hello.replace(wsRegex, ""); // Change this line
